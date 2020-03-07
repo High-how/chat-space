44d1ca9ae8b25e2e3b2fe614890d7a0841b070ce
High-how <1215ikkou@gmail.com>
2020-03-07 20:36:42 +0900

    Html-append

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,61 @@
+$(function(){
+  function buildHTML(message){
+    if (message.image) {
+      var html = 
+      `<div class="message__box">
+        <div class="message__box__info">
+          <div class="message__box__info__talker">
+            ${message.user_name}
+          </div>
+          <div class="message__box__info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message__box__text">
+          <p class="message__box__content">
+            ${message.content}
+          </p>
+        </div>
+        <img src=${message.image} >
+      </div>`
+      return html
+    } else {
+      var html = 
+      `<div class="message__box">
+        <div class="message__box__info">
+          <div class="message__box__info__talker">
+            ${message.user_name}
+          </div>
+          <div class="message__box__info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message__box__text">
+          <p class="message__box__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
+  $('#new_message').on('submit',function(e){
+    e.preventDefault()
+    var formData = new FormData (this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);      
+      $('form')[0].reset();
+    })
+  })
+});
