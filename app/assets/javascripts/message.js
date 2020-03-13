11b51856bbe5326464fe48da15951baf801064d5
High-how <1215ikkou@gmail.com>
2020-03-13 20:57:40 +0900

    data-display

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,30 +1,25 @@
 $(function(){
-  var last_message_id = $('.message__box:last').data("message-id");
-  console.log (last_message_id);
+  var buildHTML = function(message) {
+    if (message.content && message.image) {
+      var html = `<div class="message__box" data-message-id="${message.id}">
+        <div class="message__box__info">
+          <div class="message__box__info__talker">
+            ${message.user_name}
+          </div>
+          <div class="message__box__info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message__box__text">
+          <p class="message__box__content">
+            ${message.content}
+          </p>
+          <img src=${message.image} " class="lower-message__image">
+        </div>
+      </div>`
 
-  function buildHTML(message){
-    if (message.image) {
-      var html = 
-      `<div class="message__box">
-        <div class="message__box__info">
-          <div class="message__box__info__talker">
-            ${message.user_name}
-          </div>
-          <div class="message__box__info__date">
-            ${message.created_at}
-          </div>
-        </div>
-        <div class="message__box__text">
-          <p class="message__box__content">
-            ${message.content}
-          </p>
-        </div>
-        <img src=${message.image} >
-      </div>`
-      return html
-    } else {
-      var html = 
-      `<div class="message__box">
+    } else if (message.content) {
+      var html = `<div class="message__box" data-message-id="${message.id}">
         <div class="message__box__info">
           <div class="message__box__info__talker">
             ${message.user_name}
@@ -39,8 +34,23 @@ $(function(){
           </p>
         </div>
       </div>`
-      return html;
+
+    } else if (message.image) {
+      var html = `<div class="message__box" data-message-id="${message.id}">
+        <div class="message__box__info">
+          <div class="message__box__info__talker">
+            ${message.user_name}
+          </div>
+          <div class="message__box__info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message__box__text">
+          <img src=${message.image} " class="lower-message__image">
+        </div>
+      </div>`
     };
+    return html;
   }
 
   $('#new_message').on('submit',function(e){
@@ -66,4 +76,25 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   })
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message__box:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.message').append(insertHTML);
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  setInterval(reloadMessages, 7000);
 });
