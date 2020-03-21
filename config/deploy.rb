aa24f54407f5308a28b14f1d44f05c7566eae369
High-how <1215ikkou@gmail.com>
2020-03-21 14:23:44 +0900

    change-deploy-false

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,18 +31,6 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
-end
-
-# secrets.yml用のシンボリックリンクを追加
-set :linked_files, %w{ config/secrets.yml }
-
-# 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
-
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
 
   desc 'upload secrets.yml'
   task :upload do
@@ -56,3 +44,8 @@ namespace :deploy do
   before :starting, 'deploy:upload'
   after :finishing, 'deploy:cleanup'
 end
+
+# secrets.yml用のシンボリックリンクを追加
+set :linked_files, %w{ config/secrets.yml }
+
+# 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
